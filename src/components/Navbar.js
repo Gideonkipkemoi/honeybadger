f917dc1b2f3995adad65822700be6ec8bf14f7f5
GideonKipkemoi <rotichgideon760@gmail.com>
2023-09-19 11:31:13 +0300

    alt to img tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ export default function Navbar(){
     return (
         <>
             <nav className="navlinks">
-                <img className="logo" src={require("../images/Squidward.jpg")} />
+                <img className="logo" src={require("../images/Squidward.jpg")} alt="Logo"/>
             </nav>
         </>
     )
